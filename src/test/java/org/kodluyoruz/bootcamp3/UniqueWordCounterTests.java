396aa7d8e9d9444cae05c92e5c287e0867dc4e7c
Fırat Eşki <frat888@gmail.com>
2020-09-19 10:41:15 +0300

    Implement tests

--- src/test/java/org/kodluyoruz/bootcamp3/UniqueWordCounterTests.java
+++ src/test/java/org/kodluyoruz/bootcamp3/UniqueWordCounterTests.java
@@ -1,5 +1,36 @@
 package org.kodluyoruz.bootcamp3;
 
-public class UniqueWordCounterTests {
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UniqueWordCounterTests {
+    @Test
+    public void countNumberOfUniqueWords_whenInputIsEmpty_shouldReturnZero() {
+        // Arrange
+        UniqueWordCounter uniqueWordCounter = new UniqueWordCounter();
+
+        // Act
+        int output = uniqueWordCounter.countNumberOfUniqueWords("");
+
+        // Assert
+        assertEquals(0, output);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testInputData.csv")
+    public void countNumberOfUniqueWords_whenAllTestInputsAreValid_shouldPass(String input, int expectedResult){
+
+        // Arrange
+        UniqueWordCounter uniqueWordCounter = new UniqueWordCounter();
+
+        // Act
+        int output = uniqueWordCounter.countNumberOfUniqueWords(input);
+
+        // Assert
+        Assertions.assertEquals(expectedResult, output);
+    }
 }
